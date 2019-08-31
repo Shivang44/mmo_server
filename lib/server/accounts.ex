03bb370221fb3eb41fc88e68c44ab53cfc61556b
Shivang Saxena <shivang.saxena@joinroot.com>
2019-08-31 19:37:49 -0400

    Deletes comments

--- lib/server/accounts.ex
+++ lib/server/accounts.ex
@@ -8,36 +8,15 @@ defmodule Server.Accounts do
   alias Server.Accounts.User
   require Logger
 
-
+  @spec email_taken_error :: <<_::88>>
   def email_taken_error, do: "Email taken"
   def invalid_password_error, do: "Invalid password"
   def account_does_not_exist_error, do: "Account does not exist"
 
-  @doc """
-  Returns the list of users.
-
-  ## Examples
-
-      iex> list_users()
-      [%User{}, ...]
-
-  """
   def list_users do
     Repo.all(User)
   end
 
-  @doc """
-  Creates a user.
-
-  ## Examples
-
-      iex> create_user(%{field: value})
-      {:ok, %User{}}
-
-      iex> create_user(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_user(email, password) do
     password_hash = Argon2.hash_pwd_salt(password)
     attrs = %{ email: email, password_hash: password_hash }
@@ -62,68 +41,4 @@ defmodule Server.Accounts do
         end
     end
   end
-
-  @doc """
-  Gets a single user.
-
-  Raises `Ecto.NoResultsError` if the User does not exist.
-
-  ## Examples
-
-      iex> get_user!(123)
-      %User{}
-
-      iex> get_user!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_user!(id), do: Repo.get!(User, id)
-
-
-  @doc """
-  Updates a user.
-
-  ## Examples
-
-      iex> update_user(user, %{field: new_value})
-      {:ok, %User{}}
-
-      iex> update_user(user, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  # def update_user(%User{} = user, attrs) do
-  #   user
-  #   |> User.changeset(attrs)
-  #   |> Repo.update()
-  # end
-
-  @doc """
-  Deletes a User.
-
-  ## Examples
-
-      iex> delete_user(user)
-      {:ok, %User{}}
-
-      iex> delete_user(user)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  # def delete_user(%User{} = user) do
-  #   Repo.delete(user)
-  # end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking user changes.
-
-  ## Examples
-
-      iex> change_user(user)
-      %Ecto.Changeset{source: %User{}}
-
-  """
-  # def change_user(%User{} = user) do
-  #   User.changeset(user, %{})
-  # end
 end
--- lib/server_web/controllers/fallback_controller.ex
+++ lib/server_web/controllers/fallback_controller.ex
@@ -6,24 +6,10 @@ defmodule ServerWeb.FallbackController do
   """
   use ServerWeb, :controller
 
-  # def call(conn, {:error, %Ecto.Changeset{} = changeset}) do
-  #   conn
-  #   |> put_status(:unprocessable_entity)
-  #   |> put_view(ServerWeb.ChangesetView)
-  #   |> render("error.json", changeset: changeset)
-  # end
-
   def call(conn, {:error, msg}) do
     conn
     |> put_status(:unprocessable_entity)
     |> put_view(ServerWeb.ErrorView)
     |> render("error.json", msg: msg)
   end
-
-  # def call(conn, {:error, :not_found}) do
-  #   conn
-  #   |> put_status(:not_found)
-  #   |> put_view(ServerWeb.ErrorView)
-  #   |> render(:"404")
-  # end
 end
