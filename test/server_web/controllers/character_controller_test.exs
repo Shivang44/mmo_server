dbe562ce9b1b3af96472225cfe6eba2f080b1d38
Shivang Saxena <shivang.saxena@joinroot.com>
2019-09-12 22:39:12 -0400

    wip

--- test/server_web/controllers/character_controller_test.exs
+++ test/server_web/controllers/character_controller_test.exs
@@ -3,94 +3,128 @@ defmodule ServerWeb.CharacterControllerTest do
 
   alias Server.Accounts
   alias Server.Accounts.Character
-
-  @create_attrs %{
-    class: "some class",
-    name: "some name",
-    user_id: 42
-  }
-  @update_attrs %{
-    class: "some updated class",
-    name: "some updated name",
-    user_id: 43
-  }
-  @invalid_attrs %{class: nil, name: nil, user_id: nil}
-
-  def fixture(:character) do
-    {:ok, character} = Accounts.create_character(@create_attrs)
-    character
-  end
+  alias Server.Accounts.User
 
   setup %{conn: conn} do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
 
-  describe "index" do
-    test "lists all characters", %{conn: conn} do
-      conn = get(conn, Routes.character_path(conn, :index))
+  @user_create_attrs %{
+    "email" => "shivangs44@gmail.com",
+    "password" => "bubbles"
+  }
+
+  defp create_user(_context) do
+    {:ok, %User{} = user} = Accounts.create_user(@user_create_attrs)
+    Accounts.authenticate(@user_create_attrs)
+    %{user: user}
+  end
+
+  defp authenticate(%{conn: conn, user: user}) do
+    put_req_header(conn, "access_token", user.access_token)
+  end
+
+  describe "when a user has characters" do
+    setup [:create_user, :authenticate]
+    test ":index returns all characters for that user" do
+
+    end
+  end
+
+  describe "when a user doesn't have characters" do
+    test ":index returns no characters" do
+      conn = get(conn, Routes.user_character_path(conn, :index))
       assert json_response(conn, 200)["data"] == []
     end
   end
 
-  describe "create character" do
-    test "renders character when data is valid", %{conn: conn} do
-      conn = post(conn, Routes.character_path(conn, :create), character: @create_attrs)
-      assert %{"id" => id} = json_response(conn, 201)["data"]
+  # @create_attrs %{
+  #   class: "some class",
+  #   name: "some name",
+  #   user_id: 42
+  # }
+  # @update_attrs %{
+  #   class: "some updated class",
+  #   name: "some updated name",
+  #   user_id: 43
+  # }
+  # @invalid_attrs %{class: nil, name: nil, user_id: nil}
 
-      conn = get(conn, Routes.character_path(conn, :show, id))
+  # def fixture(:character) do
+  #   {:ok, character} = Accounts.create_character(@create_attrs)
+  #   character
+  # end
 
-      assert %{
-               "id" => id,
-               "class" => "some class",
-               "name" => "some name",
-               "user_id" => 42
-             } = json_response(conn, 200)["data"]
-    end
+  # setup %{conn: conn} do
+  #   {:ok, conn: put_req_header(conn, "accept", "application/json")}
+  # end
 
-    test "renders errors when data is invalid", %{conn: conn} do
-      conn = post(conn, Routes.character_path(conn, :create), character: @invalid_attrs)
-      assert json_response(conn, 422)["errors"] != %{}
-    end
-  end
+  # describe "index" do
+  #   test "lists all characters", %{conn: conn} do
+  #     conn = get(conn, Routes.character_path(conn, :index))
+  #     assert json_response(conn, 200)["data"] == []
+  #   end
+  # end
 
-  describe "update character" do
-    setup [:create_character]
+  # describe "create character" do
+  #   test "renders character when data is valid", %{conn: conn} do
+  #     conn = post(conn, Routes.character_path(conn, :create), character: @create_attrs)
+  #     assert %{"id" => id} = json_response(conn, 201)["data"]
 
-    test "renders character when data is valid", %{conn: conn, character: %Character{id: id} = character} do
-      conn = put(conn, Routes.character_path(conn, :update, character), character: @update_attrs)
-      assert %{"id" => ^id} = json_response(conn, 200)["data"]
+  #     conn = get(conn, Routes.character_path(conn, :show, id))
 
-      conn = get(conn, Routes.character_path(conn, :show, id))
+  #     assert %{
+  #              "id" => id,
+  #              "class" => "some class",
+  #              "name" => "some name",
+  #              "user_id" => 42
+  #            } = json_response(conn, 200)["data"]
+  #   end
 
-      assert %{
-               "id" => id,
-               "class" => "some updated class",
-               "name" => "some updated name",
-               "user_id" => 43
-             } = json_response(conn, 200)["data"]
-    end
+  #   test "renders errors when data is invalid", %{conn: conn} do
+  #     conn = post(conn, Routes.character_path(conn, :create), character: @invalid_attrs)
+  #     assert json_response(conn, 422)["errors"] != %{}
+  #   end
+  # end
 
-    test "renders errors when data is invalid", %{conn: conn, character: character} do
-      conn = put(conn, Routes.character_path(conn, :update, character), character: @invalid_attrs)
-      assert json_response(conn, 422)["errors"] != %{}
-    end
-  end
+  # describe "update character" do
+  #   setup [:create_character]
 
-  describe "delete character" do
-    setup [:create_character]
+  #   test "renders character when data is valid", %{conn: conn, character: %Character{id: id} = character} do
+  #     conn = put(conn, Routes.character_path(conn, :update, character), character: @update_attrs)
+  #     assert %{"id" => ^id} = json_response(conn, 200)["data"]
 
-    test "deletes chosen character", %{conn: conn, character: character} do
-      conn = delete(conn, Routes.character_path(conn, :delete, character))
-      assert response(conn, 204)
+  #     conn = get(conn, Routes.character_path(conn, :show, id))
 
-      assert_error_sent 404, fn ->
-        get(conn, Routes.character_path(conn, :show, character))
-      end
-    end
-  end
+  #     assert %{
+  #              "id" => id,
+  #              "class" => "some updated class",
+  #              "name" => "some updated name",
+  #              "user_id" => 43
+  #            } = json_response(conn, 200)["data"]
+  #   end
 
-  defp create_character(_) do
-    character = fixture(:character)
-    {:ok, character: character}
-  end
+  #   test "renders errors when data is invalid", %{conn: conn, character: character} do
+  #     conn = put(conn, Routes.character_path(conn, :update, character), character: @invalid_attrs)
+  #     assert json_response(conn, 422)["errors"] != %{}
+  #   end
+  # end
+
+  # describe "delete character" do
+  #   setup [:create_character]
+
+  #   test "deletes chosen character", %{conn: conn, character: character} do
+  #     conn = delete(conn, Routes.character_path(conn, :delete, character))
+  #     assert response(conn, 204)
+
+  #     assert_error_sent 404, fn ->
+  #       get(conn, Routes.character_path(conn, :show, character))
+  #     end
+  #   end
+  # end
+
+  # defp create_character(_) do
+  #   character = fixture(:character)
+  #   {:ok, character: character}
+  # end
 end
