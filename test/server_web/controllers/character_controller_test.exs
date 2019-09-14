bb0ef2208b89f8f58dcef44ff09fe9953b68e3f5
Shivang Saxena <shivang.saxena@joinroot.com>
2019-09-14 14:35:28 -0400

    Adds test to view all characters for user

--- test/server_web/controllers/character_controller_test.exs
+++ test/server_web/controllers/character_controller_test.exs
@@ -1,5 +1,5 @@
 defmodule ServerWeb.CharacterControllerTest do
-  use ServerWeb.ConnCase
+  use ServerWeb.ConnCase, async: true
 
   alias Server.Accounts
   alias Server.Accounts.Character
@@ -8,7 +8,8 @@ defmodule ServerWeb.CharacterControllerTest do
   require IEx
 
   setup %{conn: conn} do
-    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+    Ecto.Adapters.SQL.Sandbox.allow(Server.Repo, self(), Accounts)
+    %{conn: put_req_header(conn, "accept", "application/json")}
   end
 
   @user_create_attrs %{
@@ -17,118 +18,60 @@ defmodule ServerWeb.CharacterControllerTest do
   }
 
   defp create_user(_context) do
-    {:ok, %User{} = user} = Accounts.create_user(@user_create_attrs)
+    {:ok, %User{} = _} = Accounts.create_user(@user_create_attrs)
     {:ok, authed_user} = Accounts.authenticate(@user_create_attrs)
     %{user: authed_user}
   end
 
-  defp authenticate(%{conn: conn, user: user}) do
-    put_req_header(conn, "access_token", user.access_token)
-    :ok
+  defp create_characters(%{user: user}) do
+    {:ok, %Character{} = archer} = Accounts.create_character(%{
+      "name" => "shivang",
+      "class" => "archer",
+      "user_id" => user.id
+    })
+    {:ok, %Character{} = warrior} = Accounts.create_character(%{
+      "name" => "zhinkk",
+      "class" => "warrior",
+      "user_id" => user.id
+    })
+    {:ok, %Character{} = mage} = Accounts.create_character(%{
+      "name" => "draucia",
+      "class" => "mage",
+      "user_id" => user.id
+    })
+
+    characters = Enum.map([archer, warrior, mage], fn char ->
+      %{
+        "id" => char.id,
+        "name" => char.name,
+        "class" => char.class,
+        "user_id" => char.user_id
+      }
+    end)
+
+    %{characters: characters}
   end
 
-  # describe "when a user has characters" do
-  #   setup [:create_user, :authenticate]
-  #   test ":index returns all characters for that user" do
 
-  #   end
-  # end
+  defp authenticate(%{conn: conn, user: user}) do
+    %{conn: put_req_header(conn, "access_token", user.access_token)}
+  end
+
 
   describe "when a user doesn't have characters" do
     setup [:create_user, :authenticate]
-    test ":index returns no characters" do
-      conn = get(conn, Routes.user_character_path(conn, :index))
+    test ":index returns no characters when access_token is valid", %{conn: conn, user: user} do
+      conn = get(conn, Routes.user_character_path(conn, :index, user.id))
       assert json_response(conn, 200)["data"] == []
     end
   end
 
-  # @create_attrs %{
-  #   class: "some class",
-  #   name: "some name",
-  #   user_id: 42
-  # }
-  # @update_attrs %{
-  #   class: "some updated class",
-  #   name: "some updated name",
-  #   user_id: 43
-  # }
-  # @invalid_attrs %{class: nil, name: nil, user_id: nil}
+  describe "when a user has characters" do
+    setup [:create_user, :create_characters, :authenticate]
+    test ":index returns all characters for that user", %{conn: conn, user: user, characters: expected_characters} do
+      conn = get(conn, Routes.user_character_path(conn, :index, user.id))
+      assert json_response(conn, 200)["data"] == expected_characters
+    end
+  end
 
-  # def fixture(:character) do
-  #   {:ok, character} = Accounts.create_character(@create_attrs)
-  #   character
-  # end
-
-  # setup %{conn: conn} do
-  #   {:ok, conn: put_req_header(conn, "accept", "application/json")}
-  # end
-
-  # describe "index" do
-  #   test "lists all characters", %{conn: conn} do
-  #     conn = get(conn, Routes.character_path(conn, :index))
-  #     assert json_response(conn, 200)["data"] == []
-  #   end
-  # end
-
-  # describe "create character" do
-  #   test "renders character when data is valid", %{conn: conn} do
-  #     conn = post(conn, Routes.character_path(conn, :create), character: @create_attrs)
-  #     assert %{"id" => id} = json_response(conn, 201)["data"]
-
-  #     conn = get(conn, Routes.character_path(conn, :show, id))
-
-  #     assert %{
-  #              "id" => id,
-  #              "class" => "some class",
-  #              "name" => "some name",
-  #              "user_id" => 42
-  #            } = json_response(conn, 200)["data"]
-  #   end
-
-  #   test "renders errors when data is invalid", %{conn: conn} do
-  #     conn = post(conn, Routes.character_path(conn, :create), character: @invalid_attrs)
-  #     assert json_response(conn, 422)["errors"] != %{}
-  #   end
-  # end
-
-  # describe "update character" do
-  #   setup [:create_character]
-
-  #   test "renders character when data is valid", %{conn: conn, character: %Character{id: id} = character} do
-  #     conn = put(conn, Routes.character_path(conn, :update, character), character: @update_attrs)
-  #     assert %{"id" => ^id} = json_response(conn, 200)["data"]
-
-  #     conn = get(conn, Routes.character_path(conn, :show, id))
-
-  #     assert %{
-  #              "id" => id,
-  #              "class" => "some updated class",
-  #              "name" => "some updated name",
-  #              "user_id" => 43
-  #            } = json_response(conn, 200)["data"]
-  #   end
-
-  #   test "renders errors when data is invalid", %{conn: conn, character: character} do
-  #     conn = put(conn, Routes.character_path(conn, :update, character), character: @invalid_attrs)
-  #     assert json_response(conn, 422)["errors"] != %{}
-  #   end
-  # end
-
-  # describe "delete character" do
-  #   setup [:create_character]
-
-  #   test "deletes chosen character", %{conn: conn, character: character} do
-  #     conn = delete(conn, Routes.character_path(conn, :delete, character))
-  #     assert response(conn, 204)
-
-  #     assert_error_sent 404, fn ->
-  #       get(conn, Routes.character_path(conn, :show, character))
-  #     end
-  #   end
-  # end
-
-  # defp create_character(_) do
-  #   character = fixture(:character)
-  #   {:ok, character: character}
-  # end
 end
