1f449eb49c72dadf11ddc24a9c5eade7ca486326
Shivang Saxena <shivangs44@gmail.com>
2019-10-14 23:08:09 -0400

    Adds input to input queue from channel

--- lib/server_web/channels/game_server.ex
+++ lib/server_web/channels/game_server.ex
@@ -10,10 +10,9 @@ defmodule ServerWeb.GameServerChannel do
     {:error, %{reason: "unauthorized"}}
   end
 
-  def handle_in("client_input", msg, socket) do
-    broadcast!(socket, "world_update", %{body: "world_update"})
+  def handle_in("client_input", input, socket) do
+    # broadcast!(socket, "world_update", %{body: "world_update"})
+    Server.InputQueue.push(input)
     {:reply, {:ok, %{}}, socket}
   end
-
-
 end
