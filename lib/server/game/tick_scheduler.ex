78947e1c1159e9eb6a1dcafe2f9162b86c6fd0c1
Shivang Saxena <shivangs44@gmail.com>
2019-10-20 16:43:37 -0400

    Added tick timer

--- lib/server/game/tick_scheduler.ex
+++ lib/server/game/tick_scheduler.ex
@@ -2,16 +2,17 @@ defmodule Server.TickScheduler do
     use Task
 
     @tick_rate 1
-    @time_per_tick 1000 / @tick_rate |> Decimal.from_float |> Decimal.to_integer
+    @time_per_tick 50 / @tick_rate |> Decimal.from_float |> Decimal.to_integer
 
     def start_link(_args) do
         Task.start_link(__MODULE__, :schedule_ticks, [])
     end
 
     def schedule_ticks() do
-        Server.TickExecuter.tick()
-        Process.sleep(@time_per_tick)
+        {uSec, _} = :timer.tc(Server.TickExecuter, :tick, [])
+        IO.puts "Server tick executed in #{inspect uSec / 1000} milliseconds"
+        Process.sleep(@time_per_tick) # TODO Sleep only for time left until next tick
         schedule_ticks()
-    end
+    end    
     
 end
