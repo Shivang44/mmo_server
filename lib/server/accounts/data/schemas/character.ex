79dd55e9ff48b31bfd099366c62c5d89281d6d24
Shivang Saxena <shivangs44@gmail.com>
2019-10-23 22:51:47 -0400

    Fix broken tests

--- lib/server/accounts/data/schemas/character.ex
+++ lib/server/accounts/data/schemas/character.ex
@@ -6,9 +6,9 @@ defmodule Server.Accounts.Character do
   schema "characters" do
     field :class, :string
     field :name, :string
-    field :x, :float
-    field :y, :float
-    field :z, :float
+    field :x, :float, default: 0.0
+    field :y, :float, default: 0.0
+    field :z, :float, default: 0.0
     
     belongs_to :user, User
 
@@ -17,7 +17,7 @@ defmodule Server.Accounts.Character do
 
   def changeset(character, attrs) do
     character
-    |> cast(attrs, [:name, :class, :user_id])
+    |> cast(attrs, [:name, :class, :user_id, :x, :y, :z])
     |> validate_required([:name, :class, :user_id])
     |> unique_constraint(:name)
   end
